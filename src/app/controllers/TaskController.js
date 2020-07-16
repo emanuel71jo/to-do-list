2103fd8f1f87670959ad9dcea11012e1449541dd
emanuel71jo <emanuel71jo@gmail.com>
2020-07-16 09:25:13 -0300

    Método store para criação de atividades

--- src/app/controllers/TaskController.js
+++ src/app/controllers/TaskController.js
@@ -1,6 +1,32 @@
+import * as Yup from 'yup';
+
+import Task from '../models/Task';
+import Category from '../models/Category';
+
 class TaskController {
   async store(req, res) {
-    return res.json();
+    const schema = await Yup.object().shape({
+      content: Yup.string().required(),
+      category_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const categoryExists = await Category.findOne({
+      where: { id: req.body.category_id },
+    });
+
+    if (!categoryExists) {
+      return res.status(404).json({ error: 'Category does not exist' });
+    }
+
+    const { content, category_id } = req.body;
+
+    const { id } = await Task.create({ content, category_id });
+
+    return res.json(id);
   }
 }
 
