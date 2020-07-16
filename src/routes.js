317c4f147aec9c2bc6c87ba1b726ad4c52691b2f
emanuel71jo <emanuel71jo@gmail.com>
2020-07-16 09:26:04 -0300

    Métodos stores para criações de categorias e atividades

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
 import TaskController from './app/controllers/TaskController';
+import CategoryController from './app/controllers/CategoryController';
 
 const routes = new Router();
 
 routes.post('/task', TaskController.store);
+
+routes.post('/category', CategoryController.store);
 
 export default routes;
