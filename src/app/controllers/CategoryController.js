f3baf4962bee35b948f9262abc88b1256b3cfd65
emanuel71jo <emanuel71jo@gmail.com>
2020-07-16 09:25:02 -0300

    Método store para criação de categorias

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,23 @@
+import * as Yup from 'yup';
+
+import Category from '../models/Category';
+
+class CategoryController {
+  async store(req, res) {
+    const schema = await Yup.object().shape({
+      name: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const { name } = req.body;
+
+    const { id } = await Category.create({ name });
+
+    return res.json(id);
+  }
+}
+
+export default new CategoryController();
